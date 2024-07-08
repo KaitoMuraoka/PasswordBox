f3ab9ec7e97543e0960bc50bf4f20bc9de9dd9e4
KaitoMuraoka <kaitoboshi@icloud.com>
2024-07-08 23:00:39 +0900

    初期パスワードを生成するように修正

--- PasswordBox/UI/List/Add/AddView.swift
+++ PasswordBox/UI/List/Add/AddView.swift
@@ -1,14 +1,25 @@
 import SwiftUI
 import SwiftData
+import TipKit
+
+struct FavoriteButtonTip: Tip {
+    var title: Text {
+        Text("お気に入りボタン")
+    }
+    var message: Text? {
+        Text("この記事をお気に入りすることができます🍣\nお気に入りした記事は下のタブから確認することができます！")
+    }
+}
 
 struct AddView: View {
     @State private var title: String = "sample"
     @State private var iconString: String = "key"
-    @State private var passwordString: String = "SamplePassword"
+    @State private var passwordString: String = ""
     @State private var passwordLength: Int = 4
     @Environment(\.modelContext) private var context
     @Environment(\.dismiss) private var dismiss
     private let viewModel = AddViewModel()
+    
     var body: some View {
         NavigationStack {
             PasswordFormView(title: $title, iconString: $iconString, passwordString: $passwordString, passwordLength: $passwordLength)
@@ -27,6 +38,10 @@ struct AddView: View {
                         }
                     }
                 }
+        }
+        .onAppear {
+            self.passwordString = CreatePassword.createPassword(length: passwordLength, true, true)
+            self.passwordLength = passwordString.count
         }
     }
 }
