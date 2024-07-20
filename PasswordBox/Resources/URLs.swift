5e7f2ba36f2709bacb9fab4ce0521765b792ab1f
KaitoMuraoka <kaitoboshi@icloud.com>
2024-07-20 11:23:24 +0900

    Change from WKWebView to SafariView

--- PasswordBox/Resources/URLs.swift
+++ PasswordBox/Resources/URLs.swift
@@ -2,6 +2,7 @@ import Foundation
 
 extension URL {
     struct InfomationView {
+        static let privacyPolicy = URL(string: "https://qiita.com/KaitoMuraoka/private/21637988ff296880f635")!
         static let contactURL = URL(string: "https://docs.google.com/forms/d/e/1FAIpQLSepth9cvpo71tNVjStRtxS0LFBqXkA6V21nXXb6NcQIem6FlA/viewform?usp=sf_link")!
         static let websiteURL = URL(string: "https://github.com/KaitoMuraoka/PasswordBox")!
     }
